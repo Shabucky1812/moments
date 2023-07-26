0ee8a3704b17b7a79c4966d462aee71547a38782
Shabucky1812 <shaunbuck1812@gmail.com>
2023-07-26 14:16:13 +0000

    Fix CORS error

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://moments-react-walkthrough-18-f924318def49.herokuapp.com/";
+axios.defaults.baseURL = "https://drf-api-walkthrough-18-b94666bc97ac.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
